95630a9decc5608d3cdbae83049dada1609a589b
agophurmuz <agophurmuz@gmail.com>
2020-02-15 23:40:07 -0300

    Feature/images stand info (#41)
    * added ImageLayout component
    
    * added SmartGallery for standInfo
    
    * added back button to image, complete touchable card in standList
    
    * changed size close button
    
    * added TouchableOpacity to card and small changes

---
+++ imageGalleryScreen.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import {StatusBar, TouchableOpacity, View } from 'react-native';
+import {Icon} from 'react-native-elements';
+import SmartGallery from "react-native-smart-gallery";
+import StyleCommons from './assets/styles/StyleCommons';
+
+export default class ImageGalleryScreen extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+       StatusBar.setHidden(true);
+    }
+
+    render() {
+        return (
+            <View style={StyleCommons.container}>
+                <SmartGallery
+                    images={this.props.navigation.state.params.pictures.map(function(picture) {
+                        return {
+                            uri : picture
+                        }
+                    })}
+                    // onEndReached={() => {
+                    // add more images when scroll reaches end
+                    // }}
+                    // Change this to render how many items before and after it.
+                    loadMinimal={true}
+                    loadMinimalSize={2}
+                    // Turning this off will make it feel faster
+                    // and prevent the scroller to slow down
+                    // on fast swipes.
+                    sensitiveScroll={false}
+                />
+                <TouchableOpacity style={{flex:1,alignSelf:'flex-start',top: 24,left:16,position: 'absolute'}} onPress={()=>this.props.navigation.goBack()}>
+                    <Icon color="white" name={"close"} size={24}/>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+}
