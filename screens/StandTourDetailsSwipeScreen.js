929658a05613e9b8d42adc15ddbf1a8a283feb3a
agophurmuz <agophurmuz@gmail.com>
2020-02-24 15:08:18 -0300

    Enhancenment/tours screen (#53)
    * resolved navigation swipe screen goBack() and deleted forward and replay buttons
    
    * added title and progres tour to swipe screen

--- screens/StandTourDetailsSwipeScreen.js
+++ screens/StandTourDetailsSwipeScreen.js
@@ -14,14 +14,15 @@ export default class StandTourDetailsSwipeScreen extends React.Component {
     this.state = { isDataAvailable: false};
     this.state = { data: [{}]};
     this.state = { standsDataSource:[{}]};
+    this.state = { totalStands: 0}
   }
 
   // Lifecycle events
   componentDidMount(){
     StatusBar.setHidden(true);
+    this.getNoLinesTour();
     this._subscribe = this.props.navigation.addListener('didFocus', () => {
       saveLocation();
-      this.getNoLinesTour();
     });
   }
 
@@ -82,6 +83,7 @@ export default class StandTourDetailsSwipeScreen extends React.Component {
     this.setState({
       isLoading: false,
       standsDataSource: data,
+      totalStands: data.length,
     }, function(){
     });
   }
@@ -116,7 +118,6 @@ export default class StandTourDetailsSwipeScreen extends React.Component {
   }
 
 render() {
-
   return (
     <View style={styles.container}>
       <StatusBar style={styles.container} hidden={false} backgroundColor="#609bd1" translucent={true}/>
@@ -127,21 +128,17 @@ render() {
                   <TouchableOpacity style={{flex:1,alignSelf:'flex-start',top: 24,left:16,position: 'absolute'}} onPress={() =>this.onCloseButtonPress(this.props.navigation)}>
                     <Icon color="white" name={"close"} size={24}/>
                   </TouchableOpacity>
+                  <Text style={{flex:1,alignSelf:'flex-end',top: 24,right:16,position: 'absolute', color: 'white',fontSize: 20}}>{this.state.totalStands-this.state.standsDataSource.length+1}/{this.state.totalStands}</Text>
                   <View style={styles.controllersContainer}>
                       <View style={{flex:1,width: 220}}>
                         <Icon color="white" name={"directions-walk"} size={72}/>
                         <Text style={styles.directionsLabel}>Camina 25 metros hasta el stand {this.state.standsDataSource[0].stand_number+100}</Text>
                       </View>
-                      <View style={{flex:1,flexDirection: 'row',height: 60,marginTop: 32, alignItems: 'center',alignSelf: 'center',position: 'absolute'}}>
-                        <TouchableOpacity style={styles.buttonStyle} onPress={() =>this.onForwardButtonPress}>
-                          <Icon reverse color='rgba(0,0,0,0.7)' name={"forward-10"} size={28}/>
-                        </TouchableOpacity>
+                      <View style={{flex:1,height: 60,marginTop: 32, alignItems: 'center',alignSelf: 'center',position: 'absolute'}}>
                         <TouchableOpacity style={styles.buttonStyle} onPress={() =>this.onPlayButtonPress(this.state.standsDataSource[0])}>
                           <Icon reverse color='rgba(0,0,0,0.7)' name={"play-arrow"} size={52}/>
                         </TouchableOpacity>
-                        <TouchableOpacity style={styles.buttonStyle} onPress={() =>this.onReplayButtonPress}>
-                          <Icon reverse color='rgba(0,0,0,0.7)' name={"replay-10"} size={28}/>
-                        </TouchableOpacity>
+                        <Text style={styles.titleLabel}>{this.state.standsDataSource[0].title}</Text>
                       </View>
                       <TouchableOpacity style={styles.directionsButton} onPress= {() => this.props.navigation.navigate('DirectionsScreen', {
                         target_stand:this.state.standsDataSource[0]})}>
@@ -216,6 +213,13 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
     fontWeight: 'bold',
     textAlign: 'center',
+  },
+  titleLabel: {
+    color: 'white',
+    fontSize: 26,
+    marginTop: 30,
+    textAlign: 'center',
+    fontWeight: 'bold',
   },
   overlay: {
     flex:1,
