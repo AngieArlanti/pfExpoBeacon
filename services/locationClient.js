e6cdf53dca7b42e92ce51996badc778322d38c0e
AngieArlanti <angeles.arlanti@mercadolibre.com>
2020-03-01 19:35:59 -0300

    getLocation api integration (#60)
    Co-authored-by: agophurmuz <agophurmuz@gmail.com>

--- services/locationClient.js
+++ services/locationClient.js
@@ -1,4 +1,4 @@
-import {DEVICE_PROXIMITY_SERVICE_URL} from '../assets/constants/constants';
+import {DEVICE_PROXIMITY_SERVICE_URL, GET_LOCATION_SERVICE_URL} from '../assets/constants/constants';
 import { getUniqueId } from 'react-native-device-info';
 import {startRangingBeacons} from '../services/beaconManagerClient';
     
@@ -29,24 +29,31 @@ import {startRangingBeacons} from '../services/beaconManagerClient';
       startRangingBeacons(saveDeviceProximity);
     };
 
-    const getLocation = () => {
-      //TODO saveLocation should be inside getLocation in backend.
-      saveLocation();
-
-      //TODO call service api call when available.
-      //startRangingBeacons(getLocationApiCall);
-
-      //TODO this is fakeLocation Replace it with service when available.
-      return  [{
-        id: "ldksfjdslkf",
-        center: {
-          latitude: -34.6403200,
-          longitude: -58.401555,
+    const getLocationApiCall = (beacons) => {
+      return fetch(GET_LOCATION_SERVICE_URL, {
+        method: 'POST',
+        headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
         },
-        radius: 1,
-      }];
+        body: JSON.stringify({
+          device_id: getUniqueId(),
+          nearby_stands: getNearbyStands(beacons)
+        }),
+      })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        return responseJson
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    };
+
+    const getLocation = () => {
+      return startRangingBeacons(getLocationApiCall);
     };
 
 module.exports = {
-    saveLocation, getLocation
+    saveLocation, getLocation, getNearbyStands
 };
