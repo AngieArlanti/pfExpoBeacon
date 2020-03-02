fafeb571f60ab81afb200000c003da6d84a4115e
agophurmuz <agophurmuz@gmail.com>
2020-03-01 22:09:41 -0300

    arranged warning heat map api call (#61)
    * arranged warning heat map api call
    
    * fix
    
    Co-authored-by: Mauricio Minestrelli <mauricio.minestrelli@mercadolibre.com>

--- components/MapComponentView.js
+++ components/MapComponentView.js
@@ -13,8 +13,7 @@ import StandMarker from './StandMarker';
 import TourMarker from './TourMarker';
 import LocationMarker from './LocationMarker';
 import HorizontalCardGallery from './HorizontalCardGallery';
-import {GET_LOCATION_SERVICE_URL, DEFAULT_MAP_MARKERS_PADDING,LATITUDE,LONGITUDE, LATITUDE_DELTA,LONGITUDE_DELTA,POLYLINE_DEFAULT_STROKE_WIDTH,POLYLINE_TOUR_DEFAULT_STROKE_WIDTH,mapProperties} from '../assets/constants/constants'
-import {getHeatMap} from '../services/heatMapClient';
+import {HITS,DEFAULT_MAP_MARKERS_PADDING,LATITUDE,LONGITUDE, LATITUDE_DELTA,LONGITUDE_DELTA,POLYLINE_DEFAULT_STROKE_WIDTH,POLYLINE_TOUR_DEFAULT_STROKE_WIDTH,mapProperties, HEAT_MAP_SERVICE_URL} from '../assets/constants/constants'
 import {startRangingBeacons} from '../services/beaconManagerClient';
 import { getUniqueId } from 'react-native-device-info';
 import {getNearbyStands} from '../services/locationClient';
@@ -131,22 +130,33 @@ export default class MapComponentView extends React.Component {
     this.getLocation();
   }
 
-  //Display heatmap
+  onDirectionsButtonPress = e =>{
+    this.locateGuy(false);
+  }
+
+  getHeatMap() {
+    return fetch(HEAT_MAP_SERVICE_URL)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          heatmapWeightedLatLngs: responseJson.map(function(location){return {
+            latitude: location.latitude,
+            longitude: location.longitude,
+            weight:1
+          }})
+        })
+      })
+      .catch((error) =>{
+        this.showSnackbar();
+      });
+  };
   onLayersButtonPressed = e =>{
     if(!this.state.layerButtonPressed){
-      getHeatMap().then(data => {
+      this.getHeatMap()
         this.setState({
           showHeatMap:true,
         layerButtonPressed:true,
-          heatmapWeightedLatLngs: data.map(function(location){return {
-            latitude: location.latitude,
-            longitude: location.longitude,
-            weight:100
-          }})
         })
-      }).catch((error) =>{
-          this.showSnackbar();
-        });
     } else {
       this.setState({
         showHeatMap:false,
